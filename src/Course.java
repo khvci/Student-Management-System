6bdea56e165c6d6da713192aea499985d1020fec
R. Cem Kahveci <88493840+khvci@users.noreply.github.com>
2021-12-29 06:57:11 +0300

    add main methods

--- src/Course.java
+++ src/Course.java
@@ -3,7 +3,7 @@ public class Course {
     String courseCode;
     int midtermResult;
     int finalResult;
-    String courseTutor;
+    Tutor courseTutor;
     double courseAverage;
     boolean isCoursePassed;
 
@@ -15,5 +15,16 @@ public class Course {
         this.finalResult = -1;
         this.courseAverage = (this.midtermResult * 0.2 + this.finalResult * 0.8);
         this.isCoursePassed = this.courseAverage >= 55;
+    }
+
+    public void addTutor(Tutor t)
+    {
+        if (this.courseCode.equals(t.branch))
+        {
+            this.courseTutor = t;
+            System.out.println(this.courseTutor + " is assigned to " + this.courseName);
+        } else {
+        System.out.println(t + " can't be assigned to " + this.courseName);
+    }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,31 @@
 public class Main {
+    public static void main(String[] args) {
+        Course math101 = new Course("Math 101", "MAT101");
+        Course science101 = new Course("Science 101", "SCI101");
+        Course arts101 = new Course("Science 101", "ART101");
+
+        Tutor mathTutor = new Tutor("Leonhard Euler", "MAT101");
+        Tutor scienceTutor = new Tutor("Celal Şengör", "SCI101");
+        Tutor artsTutor = new Tutor("Jean-Michel Basquiat", "ART101");
+
+        math101.addTutor(mathTutor);
+        science101.addTutor(scienceTutor);
+        arts101.addTutor(artsTutor);
+
+        Student student1 = new Student("Jane Doe", "1001", 1, math101, science101, arts101);
+        student1.addMidtermResults(52, 27 ,41);
+        student1.addFinalResults(60,58,45);
+
+
+        Student student2 = new Student("John Doe", "1002", 1, math101, science101, arts101);
+        student2.addMidtermResults(70,68,92);
+        student2.addFinalResults(81,71,78);
+
+
+        Student student3 = new Student("Dave Doe", "1003", 1, math101, science101, arts101);
+        student2.addMidtermResults(32,25,28);
+        student2.addFinalResults(44,54,52);
+
+    }
 
 }
